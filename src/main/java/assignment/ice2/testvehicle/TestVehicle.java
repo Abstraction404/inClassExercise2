e8c1a92add096c6cdcd44710f875c870c146cec3
Admin <Abstraction404>
2025-02-10 16:51:11 -0500

    Second commit

--- src/main/java/assignment/ice2/testvehicle/TestVehicle.java
+++ src/main/java/assignment/ice2/testvehicle/TestVehicle.java
@@ -10,7 +10,21 @@ package assignment.ice2.testvehicle;
  */
 public class TestVehicle {
 
+    
+    //This is a comment, lmao
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Starting...");
+        
+        Vehicle vehicle1 =  new Vehicle("test", "diesel", "blue");
+        Car car1 =  new Car("test1", "diesel", "green");
+        Bus bus1 =  new Bus("test2", "diesel", "yellow");
+        Truck truck1 =  new Truck("test", "diesel", "blue", 100);
+        
+        System.out.println(vehicle1.toString());
+        System.out.println(car1.toString());
+        System.out.println(bus1.toString());
+        System.out.println(truck1.toString());
+        
     }
 }
